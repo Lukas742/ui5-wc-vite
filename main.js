f83dfa281ba93f39b89d15240ab47eb4831892f2
Lukas Harbarth <lukas.harbarth@sap.com>
2024-05-13 17:07:17 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+import '@ui5/webcomponents/dist/Assets.js';
+// import '@ui5/webcomponents-localization/dist/features/calendar/Gregorian.js';
+// import '@ui5/webcomponents-localization/dist/features/calendar/Buddhist.js';
+// import '@ui5/webcomponents-localization/dist/features/calendar/Islamic.js';
+// import '@ui5/webcomponents-localization/dist/features/calendar/Japanese.js';
+// import '@ui5/webcomponents-localization/dist/features/calendar/Persian.js';
+import '@ui5/webcomponents/dist/DatePicker.js';
+
